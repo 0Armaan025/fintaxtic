b35077221f562c83f996f1d6588f227882febd3e
0Armaan025 <armaan33000@gmail.com>
2025-11-14 18:25:52 +0530

    || update -> im fed up with orders and inhospitalitiy ||

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,4 @@
 import React from "react";
-import Landing from "../components/landing";
-import Features from "../components/features";
-import Pricing from "../components/pricing";
 import RickrollLanding from "../components/landing";
 
 const HomePage = () => {
