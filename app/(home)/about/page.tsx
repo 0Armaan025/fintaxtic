0c3075a90168efe5a0c32339ae16750a2d7ac2fd
0Armaan025 <armaan33000@gmail.com>
2026-08-06 21:32:37 +0530

    || update -> removed team stufft ||

--- app/(home)/about/page.tsx
+++ app/(home)/about/page.tsx
@@ -36,31 +36,31 @@ export default function About() {
       </section>
 
       {/* Team Section */}
-      <section className="max-w-5xl mx-auto">
-        <h2 className="text-3xl font-semibold text-center mb-10">
-          Meet the Team
-        </h2>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-          {team.map((member) => (
-            <div
-              key={member.name}
-              className="flex flex-col items-center text-center border rounded-2xl p-6 shadow-sm hover:shadow-md transition"
-            >
-              <div className="w-32 h-32 mb-4 relative rounded-full overflow-hidden">
-                <Image
-                  src={member.image}
-                  alt={member.name}
-                  fill
-                  className="object-top object-cover"
-                />
-              </div>
-              <h3 className="text-xl font-semibold">{member.name}</h3>
-              <p className="text-sm text-gray-500">{member.position}</p>
-              <p className="mt-3 italic text-gray-600">“{member.quote}”</p>
-            </div>
-          ))}
-        </div>
-      </section>
+      {/* <section className="max-w-5xl mx-auto"> */}
+      {/*   <h2 className="text-3xl font-semibold text-center mb-10"> */}
+      {/*     Meet the Team */}
+      {/*   </h2> */}
+      {/*   <div className="grid grid-cols-1 md:grid-cols-3 gap-8"> */}
+      {/*     {team.map((member) => ( */}
+      {/*       <div */}
+      {/*         key={member.name} */}
+      {/*         className="flex flex-col items-center text-center border rounded-2xl p-6 shadow-sm hover:shadow-md transition" */}
+      {/*       > */}
+      {/*         <div className="w-32 h-32 mb-4 relative rounded-full overflow-hidden"> */}
+      {/*           <Image */}
+      {/*             src={member.image} */}
+      {/*             alt={member.name} */}
+      {/*             fill */}
+      {/*             className="object-top object-cover" */}
+      {/*           /> */}
+      {/*         </div> */}
+      {/*         <h3 className="text-xl font-semibold">{member.name}</h3> */}
+      {/*         <p className="text-sm text-gray-500">{member.position}</p> */}
+      {/*         <p className="mt-3 italic text-gray-600">“{member.quote}”</p> */}
+      {/*       </div> */}
+      {/*     ))} */}
+      {/*   </div> */}
+      {/* </section> */}
     </div>
   );
 }
